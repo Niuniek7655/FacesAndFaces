6c1e3d953c24a90070633874f5219597a60f555c
Jan Kowalski <Niuniek7655@interia.pl>
2022-07-02 13:14:30 +0200

    lesson 8

---
+++ FacesAndFaces/FaceApiTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using FaceApiTest;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+var imagePath = @"oscars-2017.jpg";
+var urlAddress = "http://localhost:6000/api/faces";
+ImageUtility imgUtil = new ImageUtility();
+var bytes = imgUtil.ConvertToBytes(imagePath);
+List<byte[]> faceList = null;
+var byteContent = new ByteArrayContent(bytes);
+byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+using (var httpClient = new HttpClient())
+{
+    using (var response = await httpClient.PostAsync(urlAddress, byteContent))
+    {
+        string apiResponse = await response
+            .Content
+            .ReadAsStringAsync();
+        faceList = JsonConvert.DeserializeObject<List<byte[]>>(apiResponse);
+    }
+
+    if(faceList.Count > 0)
+    {
+        for(int i = 0; i < faceList.Count; i++)
+        {
+            imgUtil.FromBytesToImage(faceList[i], "face" + i);
+        }
+    }
+}
