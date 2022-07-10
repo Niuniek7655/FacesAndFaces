d11605c9e09a43f4d4f5d2cfdbfd33089ee17cd2
Jan Kowalski <Niuniek7655@interia.pl>
2022-07-10 20:05:38 +0200

    lesson 16

---
+++ FacesAndFaces/Faces.WebMvc/Program.cs
@@ -0,0 +1,51 @@
+using Faces.WebMvc.Services;
+using MassTransit;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddMassTransit(EmptyConfiguration);
+
+builder
+    .Services
+    .AddSingleton(provider => Bus.Factory.CreateUsingRabbitMq(
+        cfg =>
+        {
+            cfg.Host("localhost", "/", h => { });
+
+            builder
+            .Services
+            .AddSingleton<IBusControl>(provider => provider.GetRequiredService<IBusControl>());
+
+            builder
+            .Services
+            .AddSingleton<IHostedService, BusService>();
+        }));
+builder
+    .Services
+    .AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+void EmptyConfiguration(IBusRegistrationConfigurator configuration)
+{
+
+}
+
---
+++ FacesAndFaces/Messaging.Interfaces.Constants/Constants/RabbitMqMassTransitConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace Messaging.Interfaces.Constants.Constants
+{
+    public class RabbitMqMassTransitConstants
+    {
+        public const string RabbitMquri = "rabbitmq://rabbitmq:5672";
+        public const string UserName = "guest";
+        public const string Password = "guest";
+        public const string RegisterOrderServiceQueue = "register.order.command";
+    }
+}
